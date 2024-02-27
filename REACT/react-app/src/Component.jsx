8c641ea4982167c0acf8a79839e84ca34da8e48e
Elameen Daiyabu <elameendaiyabu@gmail.com>
2024-02-27 13:51:44 +0100

    commit

--- REACT/react-app/src/Component.jsx
+++ REACT/react-app/src/Component.jsx
@@ -1,36 +1,51 @@
 import React, { useState } from "react";
 
 function Component() {
-  const [delivery, setDelivery] = useState("");
 
-  function renderDelivery(event) {
-    setDelivery(event.target.value);
+  const [cars, setCars] = useState([])
+  const [year, setYear] = useState(2024)
+  const [make, setMake] = useState("")
+  const [model, setModel] = useState("")
+
+  function addCar() {
+    const newCar = {
+      carYear: year,
+      carMake: make,
+      carModel: model
+    }
+
+    setCars(c => [...c, `${newCar.carYear} ${newCar.carMake} ${newCar.carModel}`])
+
+    setYear(2024);
+    setMake("");
+    setModel("");
   }
+
+
+  function yearFn(event) {
+    setYear(event.target.value)
+  }
+
+  function makeFn(event) {
+    setMake(event.target.value)
+  }
+
+  function modelFn(event) {
+    setModel(event.target.value)
+  }
+
 
   return (
     <>
-      <p>Choose delivery mode</p>
-      <label htmlFor="">
-        <input
-          type="radio"
-          value="Home Delivery"
-          checked={delivery === "Home Delivery"}
-          onChange={renderDelivery}
-        />
-        Home Delivery
-      </label>
+      <h1>List of Car Objetcs</h1>
+      <ul>
+        {cars.map((car, index) => <li key={index}>{car}</li>)}
+      </ul>
+      <input type="number" value={year} onChange={yearFn} placeholder="Enter Year" />
+      <input type="text" value={make} onChange={makeFn} placeholder="Enter Make" />
+      <input type="text" value={model} onChange={modelFn} placeholder="Enter Model" />
       <br />
-      <label htmlFor="">
-        <input
-          type="radio"
-          value="Pickup"
-          checked={delivery === "Pickup"}
-          onChange={renderDelivery}
-        />
-        Pickup
-      </label>
-
-      <p>Method: {delivery}</p>
+      <button onClick={addCar}>Add Car</button>
     </>
   );
 }
