287a40ae79785aaf8ceee334fe6826b616e74372
Elameen Daiyabu <elameendaiyabu@gmail.com>
2024-03-02 14:19:32 +0100

    commit

--- REACT/digital-clock/src/DigitalClock.jsx
+++ REACT/digital-clock/src/DigitalClock.jsx
@@ -1,8 +1,34 @@
-function DigitalClock() {
+import { useState, useEffect } from "react"
 
-    return (<>
-        <p>hey</p>
-    </>
+function DigitalClock() {
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+        const interval = setInterval(() => { setTime(new Date()) }, 1000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
+    function currentTime() {
+        let hours = time.getHours()
+        const minutes = time.getMinutes()
+        const seconds = time.getSeconds()
+        const merediam = hours >= 12 ? "PM" : "AM"
+
+        hours = hours % 12 || 12
+
+        return (`${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${merediam}`)
+    }
+
+    function padZero(number) {
+        return ((number < 10 ? "0" : "") + number)
+    }
+
+    return (<div className="container">
+        <h1>{currentTime()}</h1>
+    </div>
     )
 }
 
